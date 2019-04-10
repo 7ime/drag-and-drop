bb1c44cf8aa7a0b55de72be4362d0927487ec22e
7ime <lopatsin1990@gmail.com>
2019-04-10 23:40:49 +0300

    [FE] add preloader

--- frontend/src/components/app/app.js
+++ frontend/src/components/app/app.js
@@ -3,6 +3,16 @@ import React, { Component } from 'react';
 import Board from '../board';
 
 class App extends Component {
+    componentDidMount() {
+        this.removePreloader();
+    }
+
+    removePreloader() {
+        const body = document.querySelector('body');
+        const preloader = document.getElementById('preloader');
+        body.removeChild(preloader);
+    }
+
     render() {
         return(
             <div className="wrapper">
