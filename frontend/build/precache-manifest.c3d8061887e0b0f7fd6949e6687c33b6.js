cf0c68b45cee0d9abf3ef1eb383ea61f7cd4c66b
7ime <lopatsin1990@gmail.com>
2019-04-11 11:25:02 +0300

    [FE] create build

---
+++ frontend/build/precache-manifest.c3d8061887e0b0f7fd6949e6687c33b6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a3bf7acae14d9b5ed5a88458106b58b",
+    "url": "./static/media/RobotoMedium.9a3bf7ac.woff"
+  },
+  {
+    "revision": "8eb514eccc51c61dab50",
+    "url": "./static/css/main.663f897f.chunk.css"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c5d7cd2eae31aef22508",
+    "url": "./static/js/2.f8458e02.chunk.js"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "./static/media/RobotoRegular.94dac78e.woff"
+  },
+  {
+    "revision": "08cb8f79715774f9a6285ee7db2919a3",
+    "url": "./static/media/RobotoBold.08cb8f79.woff"
+  },
+  {
+    "revision": "fdd88b742a9f6304f05b21b86ba58d9a",
+    "url": "./static/media/RobotoBlack.fdd88b74.woff"
+  },
+  {
+    "revision": "8eb514eccc51c61dab50",
+    "url": "./static/js/main.5d9dfd10.chunk.js"
+  },
+  {
+    "revision": "10ad0f861c0c5807734017c341940649",
+    "url": "./static/media/RobotoLight.10ad0f86.woff"
+  },
+  {
+    "revision": "fd5970128586cba8e7ed994f134ec176",
+    "url": "./static/media/RobotoItalic.fd597012.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "c5d7cd2eae31aef22508",
+    "url": "./static/css/2.296f96f3.chunk.css"
+  },
+  {
+    "revision": "2ef7d78b8661326720145a6e1a656c94",
+    "url": "./index.html"
+  }
+];
