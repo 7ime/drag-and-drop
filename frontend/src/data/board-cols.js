317ab0d4fc5d44e0004a7e7a89e25fb478d0a4a7
7ime <lopatsin1990@gmail.com>
2019-04-11 12:05:54 +0300

    [FE] last col always is empty

--- frontend/src/data/board-cols.js
+++ frontend/src/data/board-cols.js
@@ -4,14 +4,19 @@ import getCards from './board-cards';
 const arrNameCols = ['To do', 'In progress', 'Ready for testing', 'Tested', 'Done'];
 
 
-const arrCols = arrNameCols.map((item) => {
+const arrCols = arrNameCols.map((item, idx, arr) => {
     const prefix = item.split(' ').join('_').toLocaleLowerCase();
     const newItem = {};
 
     newItem.id = 'col_' + prefix;
     newItem.title = item;
 
-    newItem.cards = getCards(randomInteger(2, 15), prefix);
+   
+    if(idx === arr.length-1) {
+        newItem.cards = [];
+    } else {
+        newItem.cards = getCards(randomInteger(2, 15), prefix);
+    }
 
     return newItem;
 });
